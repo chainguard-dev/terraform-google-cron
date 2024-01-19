a6681710690c219d448c91e1ff29c8bf2b58463f
James Rawlings <jrawlings@chainguard.dev>
2024-01-19 16:23:40 +0000

    expose cloud run timeout (#20)
    Signed-off-by: James Rawlings <jrawlings@chainguard.dev>

--- main.tf
+++ main.tf
@@ -45,6 +45,7 @@ resource "google_cloud_run_v2_job" "job" {
       execution_environment = var.execution_environment
       service_account       = var.service_account
       max_retries           = var.max_retries
+      timeout               = var.timeout
       containers {
         image = ko_build.image.image_ref
 
